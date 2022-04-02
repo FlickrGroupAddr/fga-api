ced992fc92721198abd56b9e3f9bfbaea1193758
Terry Ott <Terry.Ott@gmail.com>
2022-04-02 18:42:37 +0000

    Creating flickr handle

--- deploy/aws/us-east-2/flickr-proxy.py
+++ deploy/aws/us-east-2/flickr-proxy.py
@@ -48,20 +48,44 @@ def _read_event(event):
 
 
 def _get_flickr_user_creds( user_cognito_id ):
-    user_creds = None
+    return _read_value_from_ssm( f"/flickrgroupaddr/user/{user_cognito_id}/secrets/flickr" )
+
+
+def _get_flickr_app_creds():
+    return _read_value_from_ssm( "/flickrgroupaddr/resources/flickr/fga_app_api_key" )
+
+
+def _read_value_from_ssm( path_string ):
+    ssm_response = None
 
     try:
-        user_creds = json.loads(
-            ssm.get_parameter(Name=f"/flickrgroupaddr/user/{user_cognito_id}/secrets/flickr_oauth_tokens" )['Parameter']['Value'] )
-
-        logger.info( f"Cognito user ID {user_cognito_id} successfully returned flickr creds from Param Store" )
-        logger.debug( json.dumps(user_creds, indent=4, sort_keys=True) )
-
+        ssm_response = json.loads(
+            ssm.get_parameter(Name=path_string)['Parameter']['Value'] )
 
     except:
-        logger.error(f"Exception thrown when trying to pull user creds for cognito user {user_cognito_id}" )
+        logger.error(f"Exception thrown when trying to read SSM data at path {path_string}" )
 
-    return user_creds
+    return ssm_response
+
+
+def _create_flickr_api_handle( app_flickr_api_key_info, user_flickr_auth_info ):
+    # Create an OAuth User Token that flickr API library understands
+    api_access_level = "write"
+    flickrapi_user_token = flickrapi.auth.FlickrAccessToken(
+        user_flickr_auth_info['user_oauth_token'],
+        user_flickr_auth_info['user_oauth_token_secret'],
+        api_access_level,
+        user_flickr_auth_info['user_fullname'],
+        user_flickr_auth_info['username'],
+        user_flickr_auth_info['user_nsid'])
+
+    flickrapi_handle = flickrapi.FlickrAPI(app_flickr_api_key_info['api_key'],
+                                           app_flickr_api_key_info['api_key_secret'],
+                                           token=flickrapi_user_token,
+                                           store_token=False,
+                                           format='parsed-json')
+
+    return flickrapi_handle
 
 
 def _process_app_requests( app_requests ):
@@ -69,10 +93,21 @@ def _process_app_requests( app_requests ):
         logger.debug( "Processing request:\n" + json.dumps(curr_request, indent=4, sort_keys=True) )
 
         
-        flickr_creds = _get_flickr_user_creds( curr_request['user_cognito_id'] )
-        if not flickr_creds:
+        flickr_creds_user = _get_flickr_user_creds( curr_request['user_cognito_id'] )
+        if not flickr_creds_user:
             logger.warn( f"Could not find flickr creds for Cognito user ID \"{curr_request['user_cognito_id']}\", bailing" )
             continue
+
+        # Now that we know the user is valid, pull FGA's app auth info 
+        flickr_creds_app = _get_flickr_app_creds()
+
+        logger.debug( "Successfully retrieved Flickr user and app creds from Parameter Store" )
+
+        flickrapi_handle = _create_flickr_api_handle( flickr_creds_app, flickr_creds_user )
+
+        logger.debug( "Successfully created Flickr API handle with app & user creds" )
+
+
 
 
 def attempt_flickr_group_add(event, context):
