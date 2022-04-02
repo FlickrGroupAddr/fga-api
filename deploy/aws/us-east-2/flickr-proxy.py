9bedccab5e9b80d046518469a94097b62ca01f63
Terry Ott <Terry.Ott@gmail.com>
2022-04-02 17:16:56 +0000

    Now getting debug logs!

--- deploy/aws/us-east-2/flickr-proxy.py
+++ deploy/aws/us-east-2/flickr-proxy.py
@@ -1,15 +1,14 @@
 import json
+import boto3
+import logging
 
+
+logging_level = logging.DEBUG
+
+logger = logging.getLogger()
+logger.setLevel(logging_level)
 
 def attempt_flickr_group_add(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
+    logger.debug( "Inside lambda function" )
+    logger.debug( json.dumps( event, indent=4, sort_keys=True) )
+    #print( json.dumps( event, indent=4, sort_keys=True ) )
